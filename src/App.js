9f10ab3f7374acaf38429f8f8e2a0ce4471f3c4c
Dylan Thomas <thomasdylan76@gmail.com>
2021-08-10 17:25:03 -0600

    Lyric Search App
    Find the lyrics to your favorite songs!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from "./components/layout/Navbar";
+import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+
+import { Provider } from './context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div class="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
   );
 }
 
